5bfa2a86c9f202bd430e52eecf19f6c80e774fda
carlos24 <carloseur24@gmail.com>
2024-09-08 15:31:04 -0400

    feature: web-socket implementation

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,7 @@
 import "./bootstrap";
 import { createApp } from "vue";
 import Home from "/resources/js/components/Home.vue";
+import Chat from "/resources/js/components/Chat.vue";
 import Login from "/resources/js/components/Login.vue";
 import Register from "/resources/js/components/Register.vue";
 import VueSweetalert2 from 'vue-sweetalert2';
@@ -12,6 +13,7 @@ app.use(VueSweetalert2);
 
 app.use(VueTheMask)
 app.component("home", Home);
+app.component("chat", Chat);
 app.component("login", Login);
 app.component("register", Register);
 
--- resources/views/authentication/login.blade.php
+++ resources/views/authentication/login.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Formulario de Registro</title>
+    <title>Login</title>
     @vite('resources/css/app.css')
     @vite('resources/js/app.js')
 </head>
